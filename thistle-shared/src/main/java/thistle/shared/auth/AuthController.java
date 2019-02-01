bd3919e116522bc9158b1aeb61ac3a642a84e38e
Kirill Smirnov <smirnowku@gmail.com>
2019-02-01 17:52:31 +0300

    Forward get token and refresh token requests through gateway

--- thistle-shared/src/main/java/thistle/shared/auth/AuthController.java
+++ thistle-shared/src/main/java/thistle/shared/auth/AuthController.java
@@ -1,10 +1,8 @@
 package thistle.shared.auth;
 
-import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient("auth")
 public interface AuthController {
 
     @PostMapping("/sign-up")
